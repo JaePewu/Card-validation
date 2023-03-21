e7d84f29f5aa77ce18bad9312c2aad88f25dd858
Jae <one.sweet.dreams@gmail.com>
2023-03-21 13:34:45 -0300

    commit

--- src/validator.js
+++ src/validator.js
@@ -1,34 +1,42 @@
 const validator = {
+
   isValid: function (numeroTarjeta) {
 
-    const numeroTarj= numeroTarjeta.split("");
+    const numeroTarj = numeroTarjeta.split("");
 
     let sum;
 
     const numTarjReves = numeroTarj;
-    //dar vuelta el numero con array .reverse
+
     numTarjReves.reverse();
-    //se multiplicara por 2
+
     const numeroTarjnew = numTarjReves;
-    //se hara un ciclo para que los numeros se multipliquen
 
     for (let i = 1; i < numeroTarjnew.length; i = i + 2) {
+
       numeroTarjnew [i] = numeroTarjnew [i] * 2;
 
-      if (numeroTarjnew[i] >= 10) {
-       numeroTarjnew[i] = (numeroTarjnew[i] % 10) + 1;
+      if (numeroTarjnew [i] >= i) {
+
+        numeroTarjnew[i] = (numeroTarjnew[i] % 10) + 1;
+
       }
 
       sum += parseInt(numeroTarjnew[i]);
       console.log(sum);
-    },
+      
+    }
+    
+  },
 
+  maskify: function (params) {
+    console.log(params)
+  }
 
-maskify: function (params) {
-  console.log(params)
 }
 
-}
-}
+
+
+
 
 export default validator;
