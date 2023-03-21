c7706c4607d00a4449c47b2cc239705270aa0977
Jae <one.sweet.dreams@gmail.com>
2023-03-20 23:51:55 -0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import validator from './validator.js';
 
+/*const numeroTarjeta = document.getElementById("numeroTarjeta");
+
+numeroTarjeta.addEventListener("input", function(){
+  const valor= numeroTarjeta.value;
+
+  console.log (valor);
+});*/
+
 console.log(validator);
