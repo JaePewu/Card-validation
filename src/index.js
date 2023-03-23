f996a22267f5aff6dbdcda47689c5ea0b59bb8c5
Jae <one.sweet.dreams@gmail.com>
2023-03-23 00:59:01 -0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import validator from './validator.js';
-validator.isValid("12345678912");//prueba
-validator.maskify("12345678912");
-/*const numeroTarjeta = document.getElementById("numeroTarjeta");
+//validator.isValid("12345678912");//prueba
+//validator.maskify("12345678912");
+
+const numeroTarjeta = document.getElementById("numeroTarjeta");
 
 numeroTarjeta.addEventListener("input", function(){
   const valor= numeroTarjeta.value;
 
-  console.log (valor);
-});*/
+  console.log (valor)
 
 console.log(validator);
