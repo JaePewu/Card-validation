b1add5709faa899fb3b55914acfc5ee9c1d555f1
Jae <one.sweet.dreams@gmail.com>
2023-03-26 02:29:24 -0300

    commit

--- src/index.js
+++ src/index.js
@@ -3,16 +3,28 @@ import validator from './validator.js';
 //validator.maskify("123456789");
 
 const numeroTarjeta = document.getElementById("numeroTarjeta");
-/*let valor = 0;
-numeroTarjeta.addEventListener("input", function () {
-  valor = numeroTarjeta.value;
-  console.log (valor)
-});*/
 
-document.getElementById("btnValidar").onclick = function() {validator.isValid(numeroTarjeta.value)};
 
-//document.getElementById("btnValidar").onclick = function() {validator.maskify(numeroTarjeta.value)};
+document.getElementById("btnValidar").onclick = function() {
+
+  const numeroTarj = numeroTarjeta.value;
+  //const signosLetras = "/^[0-9]+$/";
+
+  //debe tener un min de 13 y un max de 19
+  if (numeroTarj.length < 13 || numeroTarj.length > 19) {
+
+    alert ("¡ERROR!: El número de tarjeta debe contener entre 12 a 19 digitos");
+    console.log(numeroTarjeta);
+    return false
+  }else{
+    /*if (){ 
+    alert ("¡SOLO debe poseer digitos de 0 a 9!");
+    return false
+  }*/const resultado = validator.isValid(numeroTarjeta.value); console.log(resultado)}
+
+};
+
+//document.getElementById("btnValidar").onclick = function() {validator.maskify(numeroTarjeta.value)}; 
+// valida el maskify ^^^
 
 console.log(validator)
-
-//numeroTarjeta.addEventListener("keyup", (function validator.isValid) un ejemplo de como llamar a html puede ser keydown
--- src/validator.js
+++ src/validator.js
@@ -1,21 +1,28 @@
 const validator = {
   isValid: function (numeroTarjeta) {
+    //Toma el N° string entregado. (123)
     console.log(numeroTarjeta);
 
     const numeroTarj = numeroTarjeta.split("");
+    // el string es separando con comas. (1,2,3)
     console.log(numeroTarj);
-
+   
     const numTarj = numeroTarj.reverse();
+    // Toma el string separado y con un array reverse lo da vuelta. (3,2,1)
     console.log(numTarj);
 
     let suma = 0;
-    
+    //variable vacia o 0
+    //Se crea bucle, para que analice 1 a 1 string por string.
     for (let i = 0; i < numTarj.length; i++) {
 
-      numTarj[i] = parseInt(numTarj[i]) ;
+      numTarj[i] = parseInt(numTarj[i]);
+      //toma cada string y lo convierte en digitp, posicionado en cada i
       console.log(numTarj);
-      //tambien puede ser i%2!==0 indicando que es lo contrario
+      
       if (i % 2 === 0){
+        //El resto del valor i .... completar
+        //tambien puede ser i%2!==0 indicando que es lo contrario
 
         numeroTarj[i] = numeroTarj[i] * 1
         console.log(numTarj);
@@ -34,12 +41,15 @@ const validator = {
       console.log(suma);
     } 
     if (suma % 10 === 0) {
+      alert ("¡Felicidades por Tu Compra!");
       return true
     }else{
+      alert ("ERROR: Número de tarjeta NO Valida")
       return false
     }
   },
 
+  //Funcion ocultara los primeros digitos, dejando libre los ultimos 4.
   maskify: function (numeroTarjeta) {
 
     numeroTarjeta = numeroTarjeta.toString();
