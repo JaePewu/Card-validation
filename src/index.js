418cb6c1579fc3bc589e9aa726194b504a61f45f
Jae <one.sweet.dreams@gmail.com>
2023-03-25 23:47:29 -0300

    commit

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import validator from './validator.js';
 //validator.isValid("123456789");
 //validator.maskify("123456789");
 
-//const numeroTarjeta = document.getElementById("numeroTarjeta");
+const numeroTarjeta = document.getElementById("numeroTarjeta");
 /*let valor = 0;
 numeroTarjeta.addEventListener("input", function () {
   valor = numeroTarjeta.value;
   console.log (valor)
 });*/
 
-//document.getElementById("btnValidar").onclick = function() {validator.isValid(numeroTarjeta.value)};
+document.getElementById("btnValidar").onclick = function() {validator.isValid(numeroTarjeta.value)};
+
+//document.getElementById("btnValidar").onclick = function() {validator.maskify(numeroTarjeta.value)};
 
 console.log(validator)
 
