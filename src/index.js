005b4e8f56ff35a77d396a0dbdd7af69cec156a4
Jae <one.sweet.dreams@gmail.com>
2023-03-25 23:23:05 -0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import validator from './validator.js';
-validator.isValid("12345678912");//prueba
-validator.maskify("12345678912");
+//validator.isValid("123456789");
+//validator.maskify("123456789");
 
-/*const numeroTarjeta = document.getElementById("numeroTarjeta");
+//const numeroTarjeta = document.getElementById("numeroTarjeta");
+/*let valor = 0;
+numeroTarjeta.addEventListener("input", function () {
+  valor = numeroTarjeta.value;
+  console.log (valor)
+});*/
 
-numeroTarjeta.addEventListener("input", function()){
-  const valor= numeroTarjeta.value;
-}*/
-//console.log (valor)
+//document.getElementById("btnValidar").onclick = function() {validator.isValid(numeroTarjeta.value)};
 
-//console.log(validator)
+console.log(validator)
 
-//numeroTarjeta.addEventListener("keyup", (functionvalidator.isValid) un ejemplo de como llamar a html puede ser keydown
+//numeroTarjeta.addEventListener("keyup", (function validator.isValid) un ejemplo de como llamar a html puede ser keydown
