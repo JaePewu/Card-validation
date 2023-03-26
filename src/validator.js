09050956990a2b6febdd992717e049573522fcb0
Jae <one.sweet.dreams@gmail.com>
2023-03-26 19:13:25 -0300

    commit

--- src/validator.js
+++ src/validator.js
@@ -21,30 +21,38 @@ const validator = {
       console.log(numTarj);
       
       if (i % 2 === 0){
-        //El resto del valor i .... completar
+        //El resto del valor i es extrictamente igual a 0,
         //tambien puede ser i%2!==0 indicando que es lo contrario
 
         numeroTarj[i] = numeroTarj[i] * 1
+        //se multiplicara por 1
         console.log(numTarj);
       } else {
         numeroTarj[i] = numeroTarj[i] * 2 
+        //si no se multiplicara por 2
         console.log(numTarj);
 
       }
       
       if (numeroTarj[i] >= 10) {
+        //si el digito es Mayor o igual a 10
 
         numeroTarj[i] = (numeroTarj[i] % 10) + 1;
+        //Se dividira en 10 y el resto se le sumara 1
         console.log(numTarj);
       } 
       suma += numeroTarj[i];
+      //Se sumaran todos los digitos de la tarjeta
       console.log(suma);
     } 
     if (suma % 10 === 0) {
+      //Si el resto de 10 es igual a 0
       return true
+      //Es TRUE si el valor corresponde al LUHN, termina con un digito 0, o en decena
     }else{
       alert ("ERROR: Número de tarjeta NO Valida")
       return false
+      //si no saldra un alerta ya que no cumple con lo antes nombrado
     }
   },
 
